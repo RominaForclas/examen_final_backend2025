f73a8d0303aeeac0e8ac243c2c2639cec659b74d
Romina Forclas <rominagforclas@gmail.com>
2025-11-04 18:55:38 -0300

    cambio en models de cursada

--- examen_2025_2-main/gestion_academica/apps/cursada/models.py
+++ examen_2025_2-main/gestion_academica/apps/cursada/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from models import Profesor, Asignatura
 
 class Cursada(models.Model):
     ESTADOS = [
@@ -10,6 +11,9 @@ class Cursada(models.Model):
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
     estado = models.CharField(max_length=20, choices=ESTADOS, default='Activa')
+
+    profesor = models.ForeignKey('profesor.Profesor', on_delete=models.CASCADE, related_name='cursada')
+    asignatura = models.ForeignKey('asignatura.Asignatura', on_delete=models.CASCADE, related_name='cursada')
 
     def __str__(self):
         return f"{self.nombre} ({self.estado})"
